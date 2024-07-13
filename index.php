cc56e11e6fc585a7495634224471d281a3083b33
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-07-13 19:18:43 +0800

    fix: Move the homepage content inside a new external div element to make the navbar sticky

--- index.php
+++ index.php
@@ -22,47 +22,148 @@
 
 <!-- Homepage main content -->
   <div class="homepage background">
-
-    <!-- Editor's note -->
-    <div class="editors_note content">
-      <div class="editors_note_title">
-        <div class="editors_note_title_text">Editor's Note</div>
-        <div class="editors_note_hr"></div>
+    <div class="homepage content">
+      <?php
+        $sticky_class = "sticky_navbar"; 
+        include('./components/navbar.php'); 
+      ?>
+      
+      <!-- Editor's note -->
+      <div class="editors_note">
+        <div class="editors_note_title">
+          <div class="editors_note_title_text">Editor's Note</div>
+          <div class="editors_note_hr"></div>
+        </div>
+        <div class="editors_note_body">
+          <div class="editors_note_art"></div>
+          <div class="editors_note_text">
+            
+          </div>
+        </div>
+        <div class="editors_note_author">Carl Joshua Mamuri <br> University Editor</div>
       </div>
-      <div class="editors_note_body">
-        <div class="editors_note_art"></div>
-        <div class="editors_note_text">
-          
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+
+      <div class="articles">
+        <div class="articles_title">
+          <div class="articles_title_text">Articles</div>
+          <div class="articles_title_view_all_btn">View All</div>
+        </div>
+        <div class="articles_carousel">
+          <?php
+            $article_card_img_src = "./public/assets/article_card_img/women_lead_usg_amid_misogyny.jpg";
+            $article_card_title = "On the microscale: Women lead USG amid widespread misogyny";
+            $article_card_tag = "PULSE";
+            // include('./components/article_card.php'); 
+
+            $article_card_img_src = "./public/assets/article_card_img/are_student_councils_faltering.jpg";
+            $article_card_title = "Are student councils faltering?";
+            $article_card_tag = "COMMENTARY";
+            // include('./components/article_card.php');
+
+            $article_card_img_src = "./public/assets/article_card_img/is_the_usg_overstaffed_or_underutilized.jpg";
+            $article_card_title = "Is the USG overstaffed or underutilized?";
+            $article_card_tag = "COMMENTARY";
+            // include('./components/article_card.php');
+          ?>
         </div>
       </div>
-      <div class="editors_note_author">Carl Joshua Mamuri <br> University Editor</div>
     </div>
-
-
-    <!-- <div class="articles content">
-      <div class="articles_title">
-        <div class="articles_title_text">Articles</div>
-        <div class="articles_title_view_all_btn">View All</div>
-      </div>
-      <div class="articles_carousel">
-        <?php
-          $article_card_img_src = "./public/assets/article_card_img/women_lead_usg_amid_misogyny.jpg";
-          $article_card_title = "On the microscale: Women lead USG amid widespread misogyny";
-          $article_card_tag = "PULSE";
-          // include('./components/article_card.php'); 
-
-          $article_card_img_src = "./public/assets/article_card_img/are_student_councils_faltering.jpg";
-          $article_card_title = "Are student councils faltering?";
-          $article_card_tag = "COMMENTARY";
-          // include('./components/article_card.php');
-
-          $article_card_img_src = "./public/assets/article_card_img/is_the_usg_overstaffed_or_underutilized.jpg";
-          $article_card_title = "Is the USG overstaffed or underutilized?";
-          $article_card_tag = "COMMENTARY";
-          // include('./components/article_card.php');
-        ?>
-      </div>
-    </div> -->
   </div>
 </body>
 </html>
