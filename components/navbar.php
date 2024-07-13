9b4b1c0e7b7c6d19f81389d04342e8f4f2b7ec6d
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-07-13 17:49:41 +0800

    feat: Prevent the logo and hamburger icon from being draggable

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,4 @@
 <div class="navbar">
-  <img class="logo" src="./public/assets/logos/tls-horizontal-logo-white.svg">
-  <img class="hamburger" src="./public/assets/icons/hamburger.svg">
+  <img class="logo" src="./public/assets/logos/tls-horizontal-logo-white.svg" draggable="false">
+  <img class="hamburger" src="./public/assets/icons/hamburger.svg" draggable="false">
 </div>
