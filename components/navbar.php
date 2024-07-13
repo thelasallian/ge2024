d435e7973ff12a5306ab4484d63c2e4d783d1bd1
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-07-13 19:19:34 +0800

    feat: Make an optional sticky_navbar class for the second navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,4 @@
-<div class="navbar">
+<div class="navbar <?php echo $sticky_class?>">
   <img class="logo" src="./public/assets/logos/tls-horizontal-logo-white.svg" draggable="false">
   <img class="hamburger" src="./public/assets/icons/hamburger.svg" draggable="false">
 </div>
